e58773454603f376471d227cebc11ede6b55de8b
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-01-04 20:00:21 -0300

    Definimos el metodo cargarTemplate, inicializamos el atributo datos en el constructor de la clase

--- Core/Controller.php
+++ Core/Controller.php
@@ -1,7 +1,17 @@
 <?php
 
 class Controller{
-    
+    public $datos;
+
+    public function __construct()
+    {
+        $this->datos = array();
+    }
+
+    public function cargarTemplate($nombreView, $datosModel = array()){
+        $this->datos = $datosModel;
+        require 'Views/'.$nombreView.'.php';
+    }
 }
 
 ?>
