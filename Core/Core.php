d460bc699dd1f9771bea8e8b4095ab13011b5e68
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-01-04 18:53:36 -0300

    Definimos el metodo run que sirve para verificar la url

--- Core/Core.php
+++ Core/Core.php
@@ -2,7 +2,41 @@
 class Core{
     public function __construct()
     {
-        
+        $this->run();
+    }
+
+    public function run(){
+        if(isset($_GET['pag']) && !empty($_GET['pag'][0])){
+            $url = explode('/', $_GET['pag']);
+            $controller = $url[0].'Controller';
+            array_shift($url);
+            if(!empty($url[0]))
+            {
+                $metodo = $url[0];
+                array_shift($url);
+                if(count($url) > 0){
+                    $parametros = $url;
+                }
+                else{
+                    $parametros = array();
+                }
+            }
+            else{
+                $metodo = 'index';
+            }
+        }
+        else{
+            $controller = 'HomeController';
+            $metodo = 'index';
+        }
+
+        if(!file_exists('Controllers/'.$controller.'.php')){
+            $controller = 'HomeController';
+            $metodo = 'index';
+        }
+
+        $c = new $controller;
+        call_user_func_array(array($controller, $metodo), $parametros);
     }
 }
 ?>
