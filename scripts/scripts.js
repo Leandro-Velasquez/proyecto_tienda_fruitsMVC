768b5df8fbc0908869f28f04bb5b22c34ed21b68
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-01-05 21:20:57 -0300

    Agregamos funcionalidad al boton de menu para habilitar/deshabilitar el menu

---
+++ scripts/scripts.js
@@ -0,0 +1,5 @@
+const btnMenu = document.querySelector('.btn-menu');
+const menu = document.querySelector('.menu');
+btnMenu.addEventListener('click', () => {
+    menu.classList.toggle('mostrar-menu');
+})
