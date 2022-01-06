6593c64f83c75e3ff93af38a0ec5fc9d26d5a355
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-01-06 00:57:54 -0300

    Terminamos de darle estilos al header-div

--- Views/home.php
+++ Views/home.php
@@ -1,9 +1,9 @@
-    <div>
-        <p>FRESH & ORGANIC</p>
-        <h1>Delicious Seasonal Fruits</h1>
-        <div>
-            <a href="#">Fruit Collection</a>
-            <a href="#">Contact Us</a>
+    <div class="header-div">
+        <p class="header-div__p">FRESH & ORGANIC</p>
+        <h1 class="header-div__h1">Delicious Seasonal Fruits</h1>
+        <div class="header-div__div-container-link">
+            <a class="header-div__a" href="#">Fruit Collection</a>
+            <a class="header-div__a header-div__a--modified" href="#">Contact Us</a>
         </div>
     </div>
 </header>
