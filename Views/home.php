318179c7410218c4561fd5e7fbacbdf4847fad02
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-01-07 08:56:20 -0300

    Definimos la estructura html de section-service

--- Views/home.php
+++ Views/home.php
@@ -8,3 +8,29 @@
     </div>
 </header>
 <!---------->
+
+<section class="section-service">
+    <div class="container-service">
+        <div class="container-service__div">
+            <i class="fas fa-shipping-fast"></i>
+            <div>
+                <h3 class="container-service__h3">Free Shipping</h3>
+                <p class="container-service__p">When order over $75</p>
+            </div>
+        </div>
+        <div class="container-service__div">
+            <i class="fas fa-phone-volume"></i>
+            <div>
+                <h3 class="container-service__h3">24/7 Support</h3>
+                <p class="container-service__p">Get support all day</p>
+            </div>
+        </div>
+        <div class="container-service__div">
+            <i class="fas fa-sync"></i>
+            <div>
+                <h3 class="container-service__h3">Refund</h3>
+                <p class="container-service__p">Get refund within 3 days!</p>
+            </div>
+        </div>
+    </div>
+</section>
