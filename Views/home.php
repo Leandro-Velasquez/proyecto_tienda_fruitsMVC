42f67863e78b984d59b360359875159ed5e43cb2
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-01-08 16:56:44 -0300

    Terminamos de darle estilos a section-discount

--- Views/home.php
+++ Views/home.php
@@ -95,8 +95,8 @@
 </section>
 <section class="section-discount">
     <div class="section-discount__div">
-        <h2 class="section-discount__h2">December sale is on! with big <span class="section-discount__h2-span">Discount...</span></h2>
-        <p class="section-discount__p"><span class="section-discount__p-span1">Sale!<br>Upto</span><span class="section-discount__p-span2">50%</span>off</p>
+        <h2 class="section-discount__h2">December sale is<br>on!<br>with big <span class="section-discount__h2-span">Discount...</span></h2>
+        <p class="section-discount__p"><span class="section-discount__p-span1">Sale!<br>Upto</span><span class="section-discount__p-span2">50%</span><span class="section-discount__p-span3">off</span></p>
         <a class="section-discount__button" href="">Shop Now</a>
     </div>
 </section>
