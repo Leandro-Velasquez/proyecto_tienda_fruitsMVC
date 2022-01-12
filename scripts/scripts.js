b77e70606b246b13f70516fe4b3c89a996a0551a
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-01-12 14:21:16 -0300

    Agregamos un efecto scroll al nav a partir de los 990px

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,3 +3,8 @@ const menu = document.querySelector('.menu');
 btnMenu.addEventListener('click', () => {
     menu.classList.toggle('mostrar-menu');
 })
+
+const nav = document.querySelector('.nav');
+window.addEventListener('scroll', () => {
+    nav.classList.toggle('abajo', window.scrollY > 0);
+})
