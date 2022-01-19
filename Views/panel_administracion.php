abfa85693e34533473166d47a2bf0584e0c4bc75
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-01-19 02:08:55 -0300

    Definimos la estructura html del header y le dimos estilos

---
+++ Views/panel_administracion.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panel de administracion</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="../styles/styles.css">
+</head>
+<body>
+    <header class="header-adm">
+        <nav class="nav-adm">
+            <p class="nav-adm__p">Panel de administracion</p>
+            <button class="nav-adm__btn-menu btn-menu"><i class="nav-adm__i fas fa-bars"></i></button>
+            <ul class="menu-adm menu">
+                <li class="menu-adm__item"><a class="menu-adm__enlace" href="panel_administracion.php">Inicio</a></li>
+                <li class="menu-adm__item"><a class="menu-adm__enlace" href="administrar_productos.php">Administrar productos</a></li>
+                <li class="menu-adm__item"><a class="menu-adm__enlace" href="modificar_testimonios.php">Modificar testimonios</a></li>
+                <li class="menu-adm__item"><a class="menu-adm__enlace" href="administrar_noticias.php">Administrar noticias</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="container-adm">
+        
+    </div>
+    <script src="../scripts/scripts.js"></script>
+</body>
+</html>
