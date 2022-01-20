7d757f6887266c6724f76bd8b5324da790d51e8f
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-01-20 00:31:39 -0300

    Definimos el metodo getProductos de la clase ProductosModel

--- Models/ProductosModel.php
+++ Models/ProductosModel.php
@@ -8,7 +8,12 @@ class ProductosModel{
         $this->con = Conexion::getConexion();
     }
 
-    
+    public function getProductos()
+    {
+        $sql = $this->con->query("SELECT * FROM productos");
+        $productos = $sql->fetchall(PDO::FETCH_ASSOC);
+        return $productos;
+    }
 }
 
 ?>
