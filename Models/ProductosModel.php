2ebb62254490ffef0c2d47617e5e17273068dc01
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-01-20 19:45:53 -0300

    Cambiamos el nombre del metodo getProductos por getProductosSelectAll, definimos los metodos getProductosCodeNamePrice y getProductosSelectNamePriceImg

--- Models/ProductosModel.php
+++ Models/ProductosModel.php
@@ -8,9 +8,23 @@ class ProductosModel{
         $this->con = Conexion::getConexion();
     }
 
-    public function getProductos()
+    public function getProductosSelectAll()
     {
         $sql = $this->con->query("SELECT * FROM productos");
+        $productos = $sql->fetchall(PDO::FETCH_ASSOC);
+        return $productos;
+    }
+
+    public function getProductosCodeNamePrice()
+    {
+        $sql = $this->con->query("SELECT codigo_producto, nombre, precio FROM productos");
+        $productos = $sql->fetchall(PDO::FETCH_ASSOC);
+        return $productos;
+    }
+
+    public function getProductosSelectNamePriceImg()
+    {
+        $sql = $this->con->query("SELECT nombre, precio, imagen FROM productos");
         $productos = $sql->fetchall(PDO::FETCH_ASSOC);
         return $productos;
     }
