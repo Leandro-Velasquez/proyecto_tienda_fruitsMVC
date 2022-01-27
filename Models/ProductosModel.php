dd09876ffc6055d3020ed06449ae972282f2ec10
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-01-26 22:35:17 -0300

    Definimos el metodo getProductosLimitOffset de la clase ProductosModel

--- Models/ProductosModel.php
+++ Models/ProductosModel.php
@@ -28,6 +28,15 @@ class ProductosModel{
         $productos = $sql->fetchall(PDO::FETCH_ASSOC);
         return $productos;
     }
+
+    public function getProductosLimitOffset($limit, $indice)
+    {
+        $sql = $this->con->prepare('SELECT codigo_producto, nombre, precio FROM productos LIMIT :desde,:cantidadRegistros');
+        $sql->bindParam(':desde', $indice, PDO::PARAM_INT);
+        $sql->bindParam(':cantidadRegistros', $limit, PDO::PARAM_INT);
+        $sql->execute();
+        return $sql->fetchall(PDO::FETCH_ASSOC);
+    }
 }
 
 ?>
