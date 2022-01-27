720541298794a62f472b8a9281e3dfcbd2b251d8
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-01-26 22:39:06 -0300

    Definimos la funcion calcularPaginas del archivo mainPanelAdm.js

--- scripts/mainPanelAdm.js
+++ scripts/mainPanelAdm.js
@@ -5,31 +5,13 @@ btnMenu.addEventListener('click', () => {
 })
 
 
-const menuButtons = document.querySelector('.productos-en-stock__div-buttons');
-const buttonLeft = document.getElementById('buttonLeft');
-const buttonRight = document.getElementById('buttonRight');
-
-let cont = 0;
-menuButtons.addEventListener('click', (e) => {
-
-    if(e.target.id == buttonLeft.id){
-        cont -= 1;
-        const xml = new XMLHttpRequest();
-        xml.open('POST', '../Models/getProductosAjax.php');
-        xml.setRequestHeader("content-type", "application/x-www-form-urlencoded");
-        xml.addEventListener('load', () => {
-            console.log(xml.responseText);
-        })
-        xml.send(`cont=${cont}`);
-    }
-    else if(e.target.id == buttonRight.id){
-        cont += 1;
-        const xml = new XMLHttpRequest();
-        xml.open('POST', '../Models/getProductosAjax.php');
-        xml.setRequestHeader("content-type", "application/x-www-form-urlencoded");
-        xml.addEventListener('load', () => {
-            console.log(xml.responseText);
-        })
-        xml.send(`cont=${cont}`);
-    }
-})
+const calcularPaginas = (registrosXPagina) => {
+    const xml = new XMLHttpRequest();
+    xml.open('POST', '../Models/Ajax.php');
+    xml.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+    xml.addEventListener('load', () => {
+        const paginas = Math.ceil(parseInt(xml.responseText) / registrosXPagina);
+        botones(paginas, registrosXPagina);
+    });
+    xml.send('paginas=true');
+}
