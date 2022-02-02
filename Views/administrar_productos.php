32e4caf443629affddb69e6630a2fbd417be3f89
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-02-02 14:37:27 -0300

    Instanciamos un objeto de la clase App

--- Views/administrar_productos.php
+++ Views/administrar_productos.php
@@ -1,39 +1,7 @@
 <div class="productos-en-stock">
     <h2 class="productos-en-stock__h2">Productos</h2>
-    <table class="productos-en-stock__table">
-        <thead class="productos-en-stock__thead">
-            <tr>
-                <th class="productos-en-stock__th">Codigo</th>
-                <th class="productos-en-stock__th">Nombre</th>
-                <th class="productos-en-stock__th">Precio</th>
-            </tr>
-        </thead>
-        <tbody class="productos-en-stock__tbody">
-        <?php
-        /*
-            for($i = 0; $i < count($datosVariosArrays); $i++)
-            {
-                ?>
-                <tr class="productos-en-stock__tr">
-                    <?php
-                    foreach($datosVariosArrays[$i] as $key => $value)
-                    {
-                        ?>
-                        <td><?php echo ($key == 'precio')? '$' . $value: $value; ?></td>
-                        <?php
-                    }
-                    ?>
-                </tr>
-                <?php
-            }
-        */
-        ?>
-        </tbody>
-    </table>
-    <div class="productos-en-stock__div-buttons">
-        <!-- <button id="buttonLeft" class="productos-en-stock__buttons"><i class="fas fa-arrow-left"></i></button>
-        <button id="buttonRight" class="productos-en-stock__buttons"><i class="fas fa-arrow-right"></i></button> -->
-        <a id="buttonLeft" class="productos-en-stock__buttons" href=""><i class="fas fa-arrow-left"></i></a>
-        <a id="buttonRight" class="productos-en-stock__buttons" href=""><i class="fas fa-arrow-right"></i></a>
-    </div>
+    <?php
+    include_once "Class/App.php";
+    $app = new App(3, $valorBoton);
+    ?>
 </div>
