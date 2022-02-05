5d52799bc6bab47b9a5f5f01995f001656cb0a0d
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-02-05 17:19:01 -0300

    Se agregaron los atributos clasesTd y clasesTr, se definieron los metodos getClasesTd y getClasesTr de la clase Registro

--- Class/Registro.php
+++ Class/Registro.php
@@ -4,10 +4,14 @@ class registro
 {
     private $registro;
     private $datos;
+    private $clasesTr;
+    private $clasesTd;
 
-    public function __construct($registro = array())
+    public function __construct($registro = array(), $clasesTr = "", $clasesTd = "")
     {
         $this->datos = $registro;
+        $this->clasesTr = $clasesTr;
+        $this->clasesTd = $clasesTd;
     }
 
     public function armarRegistro()
@@ -17,7 +21,17 @@ class registro
         {
             array_push($campos, "<td>" . $dato . "</td>");
         }
-        $this->registro = "<tr>" . implode("", $campos) ."</tr>";
+        $this->registro = "<tr class=$this->clasesTr>" . implode("", $campos) ."</tr>";
+    }
+
+    public function getClasesTr()
+    {
+        return $this->clasesTr;
+    }
+
+    public function getClasesTd()
+    {
+        return $this->clasesTd;
     }
 
     public function getRegistro()
