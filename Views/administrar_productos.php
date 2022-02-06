685336f4373393c0d11a931ba14877f7bd2df924
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-02-06 14:19:28 -0300

    Definimos la estructura html del div agregar-productos, instanciamos el objeto AgregarProductos

--- Views/administrar_productos.php
+++ Views/administrar_productos.php
@@ -2,6 +2,17 @@
     <h2 class="productos-en-stock__h2">Productos</h2>
     <?php
     include_once "Class/App.php";
-    $app = new App(3, $valorBoton);
+    $app = new App(5, $valorBoton);
+    ?>
+</div>
+<div class="agregar-productos">
+    <h2 class="agregar-productos__h2">Agregar productos</h2>
+    <?php
+    include_once "Class/AgregarProductos.php";
+    $agregarProductos = new AgregarProductos("agregar-productos__form");
+    if(isset($_POST['codigo']))
+    {
+        $agregarProductos->agregarProductoALaBaseDeDatos($_POST, $_FILES['imagen']);
+    }
     ?>
 </div>
