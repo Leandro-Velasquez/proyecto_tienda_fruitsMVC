e2293b39b6fe4738ab8ca1e4cd71ed9efcd60017
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-02-20 22:15:44 -0300

    Definimos los metodos cart, ingresar_tienda, procesar_compra de la clase ShopController

--- Controllers/ShopController.php
+++ Controllers/ShopController.php
@@ -8,16 +8,53 @@ class ShopController extends Controller
 
     public function index($nPagina = null)
     {
+        session_start();
         $arrayParametros = array();
         if(empty($nPagina))
         {
             $arrayParametros["nPagina"] = 1;
         }
-        else
+        else if(is_numeric($nPagina))
         {
             $arrayParametros["nPagina"] = $nPagina;
         }
         $this->cargarTemplate("shop", $arrayParametros);
+    }
+
+    public function cart()
+    {
+        session_start();
+        $arrayProductos = array();
+        if(!empty($_SESSION['productos']))
+        {
+            $buscarProductos = new ProductosModel();
+            $productos = explode(",", substr($_SESSION['productos'], 0, -1));
+            $cantidad = array_count_values($productos);
+            $productos = array_unique($productos);
+            $i = 0;
+            foreach($productos as $nombreProducto)
+            {
+                array_push($arrayProductos, $buscarProductos->buscarProductoPorNombre($nombreProducto));
+
+                array_push($arrayProductos[$i], $cantidad[$nombreProducto], "$" . strval($cantidad[$nombreProducto] * $arrayProductos[$i]['precio']));
+                $i++;
+            }
+        }
+        $this->cargarTemplate("cart", array(), $arrayProductos);
+    }
+
+    public function ingresar_tienda()
+    {
+        session_start();
+        $_SESSION['productos'] = '';
+        header("Location: http://localhost/proyecto_tienda_fruitsMVC/Shop");
+    }
+
+    public function procesar_compra($producto)
+    {
+        session_start();
+        $_SESSION['productos'] .= $producto . ",";
+        header("Location: http://localhost/proyecto_tienda_fruitsMVC/Shop");
     }
 }
 ?>
